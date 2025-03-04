39abad6c8fed9f76095136ff6d2ef05dbbf93a93
Baris Tekir <trexepta@gmail.com>
2025-03-04 21:23:35 +0300

    Add spring boot project.

---
+++ Auto-service/src/test/java/service_auto_project/Auto/service/AutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package service_auto_project.Auto.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
